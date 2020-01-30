57073c3540726048aba78b88eec11945adc50957
Cecil <zfuaj@connect.ust.hk>
2020-01-30 20:42:56 +0800

    delete vendor script

--- gulpfile.js
+++ gulpfile.js
@@ -23,9 +23,8 @@ const pkg = require('./package.json');
 
 // Set the banner content
 const banner = ['/*!\n',
-  ' * TREE - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * TREE - <%= pkg.title %> v<%= pkg.version %>\n',
   ' * Copyright 2020-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
   ' */\n',
   '\n'
 ].join('');
